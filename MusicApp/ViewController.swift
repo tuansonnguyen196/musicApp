b2e461605d7e061e2ca7b866d8ed037357ab3341
tuanson <ngochieunguen196@gmail.com>
2020-07-21 10:51:55 +0700

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Nero on 7/21/20.
+//  Copyright © 2020 Nero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
